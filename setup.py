86e7af836865f37f8d6c969a7ad485a0fda8e447
Abdur Rabbi <yeasin4905@gmail.com>
2023-02-04 16:08:06 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+try:
+	import pypandoc
+	long_description = pypandoc.convert_file('README.md', 'rst')
+	long_description = long_description.replace("\r","") 
+except(IOError, ImportError):
+	long_description = open('README.md').read()
+
+setup(name='crackertool',
+version='0.1',
+description='A PIP FOR INSTALLING CRACKER-TOOL OF CRACKER911181',
+license="GNU General Public License v3.0",
+long_description=long_description,
+long_description_content_type='text/markdown',
+author='cracker911181',
+author_email='admin@cracker911181.cf',
+url='https://pypi.org/project/crackertool',
+download_url='https://pypi.org/project/crackertool',
+packages=['crackertool'],
+
+zip_safe=False)
